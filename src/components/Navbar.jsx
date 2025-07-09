aa03749096d95db459880dcf1f2a4252b360c6dd
Judith Ramírez <judithramirezpachon@gmail.com>
2025-07-09 09:50:54 +0000

    meh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,41 @@
 import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 
+
 export const Navbar = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
-return (
-	<nav className="navbar navbar-light bg-light">
-		<div className="container">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+console.log(store.favs);
+
+
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
-			</div>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
+				</div>
 
-			<div className="dropdown">
-				<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Dropdown button
-				</button>
-				<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-					<a className="dropdown-item" href="#">Action</a>
+				<div className="dropdown">
+					<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Dropdown button
+					</button>
+					<ul className="dropdown-menu">
+						{store.favs.map((item, index) => (
+						<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+							<span>{item.name}</span>
+							<button className="btn btn-sm btn-outline-danger" >×</button>
+						</li>
+					))}
 
+					</ul>
 				</div>
 			</div>
-		</div>
-	</nav>
-);
+		</nav>
+	);
 };
